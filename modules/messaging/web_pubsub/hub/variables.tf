25d2b320352c6ef346734f42c101f16a08d796e5
Francisco Robles Martín <francisco.robles@volocopter.com>
2023-01-26 11:41:21 +0100

    fix: remove duplicated/unnecessary resources/variables
    Removed duplicated resource resource_provider_registration
    Removed unneccesary variables api_management_name and resource_group_name in messaging/web_pubsub/hub module
    
    Were included while fixing conflicts in the previous commit 2661eca14d2915898e232ead813c88d124be69da

--- modules/messaging/web_pubsub/hub/variables.tf
+++ modules/messaging/web_pubsub/hub/variables.tf
@@ -11,9 +11,3 @@ variable "remote_objects" {
   description = "(Required) Specifies the supported Azure location where to create the resource. Changing this forces a new resource to be created."
   default     = {}
 }
-variable "api_management_name" {
-  description = " The Name of the API Management Service where this subscription should be created. Changing this forces a new resource to be created."
-}
-variable "resource_group_name" {
-  description = " The Name of the Resource Group where the API Management subscription exists. Changing this forces a new resource to be created."
-}
--- variables.tf
+++ variables.tf
@@ -412,6 +412,3 @@ variable "iot" {
 variable "resource_provider_registration" {
   default = {}
 }
-variable "resource_provider_registration" {
-  default = {}
-}
